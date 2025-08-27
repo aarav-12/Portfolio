653cbbea12831c640bb210d379a299ab94fecc81
Aarav Mithrani <aarav.mi33@gmail.com>
2025-08-28 03:13:11 +0530

    final changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,7 @@
 const Footer = () => {
   return (
     <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      
       <div className="text-white-500 flex gap-2">
         <p>Terms & Conditions</p>
         <p>|</p>
@@ -8,15 +9,35 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        {/* GitHub */}
+        <a 
+          href="https://github.com/aarav-12" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon block w-10 h-10"
+        >
+          <img src="/assets/github.svg" alt="github" className="w-full h-full" />
+        </a>
+
+        {/* LinkedIn */}
+        <a 
+          href="https://www.linkedin.com/in/aarav-mithrani-571181206" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon block w-6 h-6"
+        >
+          <img src="/assets/linkedin.svg" alt="linkedin" className="w-full h-full" />
+        </a>
+
+        {/* Instagram */}
+        <a 
+          href="https://instagram.com/aarav_mithrani" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-icon block w-10 h-10"
+        >
+          <img src="/assets/instagram.svg" alt="instagram" className="w-full h-full" />
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 Aarav Mithrani. All rights reserved.</p>
